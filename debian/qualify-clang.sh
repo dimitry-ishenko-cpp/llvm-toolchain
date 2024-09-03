74cc5fb9e4e2b9ac51202c38ac9892ad25e9f738
Matthias Klose <doko@ubuntu.com>
2024-09-03 14:48:07 +0200

    * debian/qualify-clang.sh: Don't check for libclang-cpp.so.$VERSION symlink.

--- debian/qualify-clang.sh
+++ debian/qualify-clang.sh
@@ -486,7 +486,7 @@ check_symlink() {
     fi
 }
 
-check_symlink "libclang-cpp.so.$VERSION"
+#check_symlink "libclang-cpp.so.$VERSION" this one is not installed
 check_symlink "libclang-$VERSION.so"
 check_symlink "libclang.so"
 
